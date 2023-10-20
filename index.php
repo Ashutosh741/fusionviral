1779c0eaf96c0c1275ae42c78dc63dac9777b4b0
Ashutosh741 <ashutoshm711btechaicse@kccitm.edu.in>
2023-10-20 16:12:45 +0530

    pages done

--- index.php
+++ index.php
@@ -104,7 +104,92 @@
         </div>
       </div>
     </section>
-
+    <!-- End FunFact -->
+    <!-- Start Service Section -->
+    <!-- <section  >
+      <div class="cs-height_150 cs-height_lg_80"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-4">
+            <div class="cs-section_heading cs-style1">
+              <h3 class="cs-section_subtitle">What Can We Do</h3>
+              <h2 class="cs-section_title">Services we can help you with</h2>
+              <div class="cs-height_45 cs-height_lg_20"></div>
+              <a href="service.php" class="cs-text_btn wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
+                <span>See All Services</span>
+                <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
+                </svg>
+              </a>
+            </div>
+            <div class="cs-height_90 cs-height_lg_45"></div>
+          </div>
+          <div class="col-xl-8">
+            <div class="row">
+              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
+              <div class="col-lg-3 col-sm-6">
+                <div class="cs-hobble">
+                  <a href="Youtube.php" class="cs-card cs-style1 cs-hover_layer1">
+                    <img src="assets/img/service_1.jpeg" alt="Service">
+                    <div class="cs-card_overlay"></div>
+                    <div class="cs-card_info">
+                      <span class="cs-hover_layer3 cs-accent_bg"></span>
+                      <h2 class="cs-card_title">YOUTUBE Promotion</h2>
+                    </div>
+                  </a>
+                </div>
+                <div class="cs-height_0 cs-height_lg_30"></div>
+              </div>
+              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
+              <div class="col-lg-3 col-sm-6">
+                <div class="cs-hobble">
+                  <a href="Instagram.php" class="cs-card cs-style1 cs-hover_layer1">
+                    <img src="assets/img/service_2.jpeg" alt="Service">
+                    <div class="cs-card_overlay"></div>
+                    <div class="cs-card_info">
+                      <span class="cs-hover_layer3 cs-accent_bg"></span>
+                      <h2 class="cs-card_title">INSTAGRAM Promotion</h2>
+                    </div>
+                  </a>
+                </div>
+                <div class="cs-height_0 cs-height_lg_30"></div>
+              </div>
+              <div class="col-lg-3 col-sm-6">
+                <div class="cs-hobble">
+                  <a href="Facebook.php" class="cs-card cs-style1 cs-hover_layer1">
+                    <img src="assets/img/service_3.jpeg" alt="Service">
+                    <div class="cs-card_overlay"></div>
+                    <div class="cs-card_info">
+                      <span class="cs-hover_layer3 cs-accent_bg"></span>
+                      <h2 class="cs-card_title">FACEBOOK Promotion</h2>
+                    </div>
+                  </a>
+                </div>
+                <div class="cs-height_0 cs-height_lg_30"></div>
+              </div>
+              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
+              <div class="col-lg-3 col-sm-6">
+                <div class="cs-hobble">
+                  <a href="Linkedin.php" class="cs-card cs-style1 cs-hover_layer1">
+                    <img src="assets/img/service_4.jpeg" alt="Service">
+                    <div class="cs-card_overlay"></div>
+                    <div class="cs-card_info">
+                      <span class="cs-hover_layer3 cs-accent_bg"></span>
+                      <h2 class="cs-card_title">LINKEDIN Promotion</h2>
+                    </div>
+                  </a>
+                </div>
+                <div class="cs-height_0 cs-height_lg_30"></div>
+              </div>
+              <div class="col-lg-3 col-sm-6 cs-hidden_mobile"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="cs-height_150 cs-height_lg_50"></div>
+    </section> -->
+    <!-- End Service Section -->
+    <div class="cs-height_85 cs-height_lg_30"></div>
   <!-- Start services Project -->
   <section id="service">
     <div class="container">
@@ -192,9 +277,71 @@
       <!-- <div class="cs-height_50 cs-height_lg_50"></div> -->
     </div>
   </section>
-  
+  <!-- End services Project -->
+
+  <!-- <div class="cs-height_85 cs-height_lg_30"></div> -->
+    <!-- Start Latest Project -->
+    <!-- <section>
+      <div class="container">
+        <div class="cs-section_heading cs-style1 text-center">
+          <h3 class="cs-section_subtitle">Latest Projects</h3>
+          <h2 class="cs-section_title wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"> Portfolio to explore </h2>
+        </div>
+      </div>
+      <div class="cs-height_90 cs-height_lg_45"></div>
+      <div class="cs-slider cs-style3 cs-gap-24">
+        <div class="cs-slider_container" data-autoplay="0" data-loop="1" data-speed="600" data-center="1" data-slides-per-view="1">
+          <div class="cs-slider_wrapper">
+            <div class="cs-slide">
+              <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-bg">
+                <div class="cs-portfolio_hover"></div>
+                <div class="cs-portfolio_bg" data-src="assets/img/portfolio_1.jpeg"></div>
+                <div class="cs-portfolio_info">
+                  <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+                  <h2 class="cs-portfolio_title">Colorful Art Work</h2>
+                  <div class="cs-portfolio_subtitle">See Details</div>
+                </div>
+              </a>
+            </div>
+            // .cs-slide //
+            <div class="cs-slide">
+              <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-bg">
+                <div class="cs-portfolio_hover"></div>
+                <div class="cs-portfolio_bg" data-src="assets/img/portfolio_2.jpeg"></div>
+                <div class="cs-portfolio_info">
+                  <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+                  <h2 class="cs-portfolio_title">Colorful Art Work</h2>
+                  <div class="cs-portfolio_subtitle">See Details</div>
+                </div>
+              </a>
+            </div>
+            // .cs-slide //
+            <div class="cs-slide">
+              <a href="portfolio-details.html" class="cs-portfolio cs-style1 cs-bg">
+                <div class="cs-portfolio_hover"></div>
+                <div class="cs-portfolio_bg" data-src="assets/img/portfolio_3.jpeg"></div>
+                <div class="cs-portfolio_info">
+                  <div class="cs-portfolio_info_bg cs-accent_bg"></div>
+                  <h2 class="cs-portfolio_title">Colorful Art Work</h2>
+                  <div class="cs-portfolio_subtitle">See Details</div>
+                </div>
+              </a>
+            </div>
+            //.cs-slide //
+          </div>
+        </div>
+        // .cs-slider_container //
+        <div class="cs-pagination cs-style1"></div>
+      </div>
+      // .cs-slider //
+    </section> -->
+    <!-- End Latest Project -->
+    <div class="cs-height_150 cs-height_lg_80"></div>
     <!-- Start Awards Text -->
     <section class="cs-shape_wrap_2">
+      <div class="cs-shape_2">
+        <div></div>
+      </div>
       <div class="container">
         <div class="cs-slider cs-style1 cs-gap-24">
           <div class="cs-slider_left">
@@ -202,6 +349,7 @@
               <h3 class="cs-section_subtitle wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s"> Our Awards </h3>
               <h2 class="cs-section_title">We get multiple awards</h2>
             </div>
+            <div class="cs-height_45 cs-height_lg_20"></div>
             <div class="cs-slider_arrows cs-style1 cs-primary_color">
               <div class="cs-left_arrow cs-center">
                 <svg width="26" height="13" viewBox="0 0 26 13" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -225,6 +373,7 @@
                     <p>Website of the day</p>
                     <p>Mobile exelence</p>
                   </div>
+                  <div class="cs-height_40 cs-height_lg_30"></div>
                   <div class="cs-time_line cs-style1">
                     <h3 class="cs-accent_color">2021</h3>
                     <h2>CSS awards design</h2>
@@ -232,6 +381,7 @@
                     <p>Desktop exelence</p>
                   </div>
                 </div>
+                <!-- .cs-slide -->
                 <div class="cs-slide">
                   <div class="cs-time_line cs-style1">
                     <h3 class="cs-accent_color">2020</h3>
@@ -239,6 +389,7 @@
                     <p>Honorable mention</p>
                     <p>Desktop exelence</p>
                   </div>
+                  <div class="cs-height_40 cs-height_lg_30"></div>
                   <div class="cs-time_line cs-style1">
                     <h3 class="cs-accent_color">2022</h3>
                     <h2>UI/UX design of the month</h2>
@@ -254,6 +405,7 @@
                     <p>Website of the day</p>
                     <p>Mobile exelence</p>
                   </div>
+                  <div class="cs-height_40 cs-height_lg_30"></div>
                   <div class="cs-time_line cs-style1">
                     <h3 class="cs-accent_color">2021</h3>
                     <h2>CSS awards design</h2>
@@ -269,6 +421,7 @@
                     <p>Honorable mention</p>
                     <p>Desktop exelence</p>
                   </div>
+                  <div class="cs-height_40 cs-height_lg_30"></div>
                   <div class="cs-time_line cs-style1">
                     <h3 class="cs-accent_color">2022</h3>
                     <h2>UI/UX design of the month</h2>
@@ -280,12 +433,14 @@
               </div>
             </div>
             <!-- .cs-slider_container -->
+            <div class="cs-pagination cs-style1 cs-hidden_desktop"></div>
           </div>
         </div>
         <!-- .cs-slider -->
       </div>
     </section>
     <!-- End Awards Text -->
+    <div class="cs-height_130 cs-height_lg_70"></div>
     <!-- Start General Text -->
     <div class="container">
       <h2 class="cs-font_50 cs-m0 text-center cs-line_height_4 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"> Our agile process is ability to adapt and respond to change. Agile organizations view change as an opportunity, not a threat. </h2>
